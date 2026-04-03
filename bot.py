70c9124a2e5d314cd683909c1f9d5a3a63a0c3e9
StrangeQuark590 <strangequarkk@gmail.com>
2026-04-03 22:35:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,51 +3,86 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTyp
 from db import get_doc, collection, get_all_docs
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
+
 TOKEN = os.getenv("BOT_TOKEN")
-async def handle_message(update : Update, context : ContextTypes.DEFAULT_TYPE):
+
+if not TOKEN:
+    raise ValueError("❌ BOT_TOKEN is missing")
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message is None:
         return
+
+    # 📂 Upload file
     if update.message.document:
         file_id = update.message.document.file_id
         context.user_data["pending_file"] = file_id
-        await update.message.reply_text("Enter a name for this file: ")
+
+        await update.message.reply_text("Enter a name for this file:")
         return
+
+    # 📝 Save file with name
     if "pending_file" in context.user_data:
+        if not update.message.text:
+            return
+
         file_id = context.user_data["pending_file"]
         name = update.message.text.lower()
+
         collection.insert_one({
-            "keyword" : name,
-            "file_path" : file_id
-        })   
+            "keyword": name,
+            "file_path": file_id
+        })
+
         context.user_data.pop("pending_file")
+
         await update.message.reply_text("File uploaded successfully")
+        return  # ✅ important
+
+    # 🔍 Search
+    if not update.message.text:
+        return
+
     text = update.message.text.lower()
     doc = get_doc(text)
+
     if doc:
         await update.message.reply_document(
-            document = doc["file_path"],
-            caption = doc["keyword"]
+            document=doc["file_path"],
+            caption=doc["keyword"]
         )
     else:
         await update.message.reply_text("Document not found")
-async def start(update, context):
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     docs = get_all_docs()
+
     if not docs:
         await update.message.reply_text("No documents available yet")
         return
-    message = "Available Documents: \n\n"
+
+    message = "📂 Available Documents:\n\n"
+
     for doc in docs:
-        message += f" {doc['keyword']}\n"
-    message += "\n Send the name to get the file"
+        message += f"• {doc['keyword']}\n"
+
+    message += "\n👉 Send the name to get the file"
+
     await update.message.reply_text(message)
 
+
 app = ApplicationBuilder().token(TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
+
 app.add_handler(
     MessageHandler(
-        (filters.TEXT & ~filters.COMMAND )| filters.Document.ALL,
-        handle_message 
+        (filters.TEXT & ~filters.COMMAND) | filters.Document.ALL,
+        handle_message
     )
 )
-app.run_polling() 
+
+app.run_polling()
