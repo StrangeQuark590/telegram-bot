7b82cb94c8611e2fc90d9cb94ff3026d185619c4
StrangeQuark590 <strangequarkk@gmail.com>
2026-04-03 23:15:56 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,27 @@
-import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 
-print("DEBUG MONGO_URI:", MONGO_URI)
+if not MONGO_URI:
+    raise ValueError(" MONGO_URI is missing")
 
 try:
-    client = MongoClient(MONGO_URI)
+    client = MongoClient(MONGO_URI, tls=True)
     client.admin.command("ping")
-    print("✅ MongoDB connected")
+    print(" MongoDB connected")
 except Exception as e:
-    print("❌ MongoDB error:", e)
+    print(" MongoDB error:", e)
     raise e
+
+db = client["telegram_bot"]
+collection = db["documents"]
+
+def get_doc(keyword):
+    return collection.find_one({"keyword": keyword})
+
+def get_all_docs():
+    return list(collection.find({}, {"_id": 0, "keyword": 1}))
